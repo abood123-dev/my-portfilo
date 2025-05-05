a4efdb1725c4f923deb130861d937146e3b3b794
Abood <abood@gmail.com>
2025-05-05 14:01:44 -0700

    Services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,48 @@
+"use client";
 import React from 'react'
-
+import {easeInOut, motion} from "framer-motion";
 const page = () => {
+  const services=
+  [
+    {
+      num:'01',
+      title:'Responsive Web Development',
+      desc:"I build fully responsive and user-friendly web applications that look great and work smoothly on all devices—from desktops to smartphones. Using Tailwind CSS, I ensure consistent design and performance across screen sizes."  
+    },
+    {
+      num:'02',
+      title:'Modern Frontend Development with React & TypeScript',
+      desc:'I develop scalable, maintainable, and high-performance user interfaces using React and TypeScript. My code is clean, well-typed, and built for long-term growth with modern development standards.'
+    },
+    {
+      num:'03',
+      title:"API Integration & Data Handling",
+      desc:'I connect web applications to external APIs, fetch and display data efficiently, and handle errors gracefully. Whether its REST or JSON-based APIs, I ensure smooth communication between the frontend and backend.'
+    },
+    {
+      num:'04',
+      title:"State Management with Redux",
+      desc:"I use Redux to manage application state, especially for larger apps that require consistent data flow. With Redux Toolkit, I organize and maintain state in a way that's easy to scale and debug."
+    }
+  ]
   return (
-    <div>
-      services
+    <>
+    <motion.div initial={{opacity:0}} animate={{opacity:1,transition:{duration:0.4,delay:2.0,ease:easeInOut}}}>
+    <div className='grid grid-cols-2 gap-[30px] items-center ml-[100px] mt-[50px] mb-10 text-white'>
+    {services.map((service,index)=>
+      {
+        return(
+         <div key={index} className='w-[450px]'>
+               <div className='text-5xl font-extrabold text-outline text-transparent'>{service.num}</div>
+               <div className='font-bold text-[35px]'>{service.title}</div>
+               <div>{service.desc}</div>
+               <div className='border-b border-white/20 w-full p-[10px]'></div>              
+         </div>
+        )
+      })}  
     </div>
+    </motion.div>
+    </>
   )
 }
 
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -2,11 +2,31 @@
 import React from 'react'
 import Image from 'next/image';
 import { easeInOut, motion } from 'framer-motion';
+import { reverse } from 'dns';
 const Photo = () => {
   return (
     <div>
         <motion.div initial={{opacity:0}} animate={{opacity:1,transition:{ease:easeInOut,duration:0.4,delay:2}}}>
-      <Image src="/IMG_٢٠٢٥٠٣٣١_١٦٢٠٠٥.jpg" alt='' width={350} height={350} className='md:w-[350px] md:h-[350px] w-[180px] h-[180px] border-none rounded-full  mix-blend-lighten object-cover md:mr-0 mr-2'/>
+      <Image src="/IMG_٢٠٢٥٠٣٣١_١٦٢٠٠٥.jpg" alt='' width={350} height={350} className='relative md:w-[350px] md:h-[350px] w-[180px] h-[180px] border-none rounded-full  mix-blend-lighten object-cover md:mr-0 mr-2'/>
+      <motion.svg className='md:w-[450px] md:h-[500px] w-[250px] h-[300px] absolute md:bottom-6 md:right-12 top-[70px] right-[67px] '
+      fill='transparent'
+      viewBox='0 0 586 586'
+      xmlns='http://www.w3.org/2000/svg'
+      >
+        <motion.circle cx='253' cy='253' r='250' stroke='#00ff99'
+        strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'
+        initial={{strokeDasharray:"24 10 0 0"}}
+        animate={{
+          strokeDasharray:["15 120 25 25","16 25 92 72","4 250 22 22"],
+          rotate:[120,360],
+        }}
+        transition={{
+          repeat:Infinity,
+          repeatType:"reverse",
+          duration:20, 
+        }}
+        />
+      </motion.svg>
         </motion.div>
     </div>
   )
