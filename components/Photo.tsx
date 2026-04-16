5d9c4f1ef9bff296c9ceb9b8cf8f4e4429c1b8e3
Abood <abood@gmail.com>
2026-04-16 07:16:19 -0700

    New Changes for the image

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -5,31 +5,50 @@ import { easeInOut, motion } from 'framer-motion';
 import { reverse } from 'dns';
 const Photo = () => {
   return (
-    <div>
-        <motion.div initial={{opacity:0}} animate={{opacity:1,transition:{ease:easeInOut,duration:0.4,delay:2}}}>
-      <Image src="/IMG_٢٠٢٥٠٣٣١_١٦٢٠٠٥.jpg" alt='' width={350} height={350} className='relative md:w-[350px] md:h-[350px] w-[180px] h-[180px] border-none rounded-full  mix-blend-lighten object-cover md:mr-0 -mr-5'/>
-      <motion.svg className='md:w-[450px] md:h-[500px] w-[250px] h-[300px] absolute md:bottom-6 md:right-12 top-[70px] right-[50px] '
-      fill='transparent'
-      viewBox='0 0 586 586'
-      xmlns='http://www.w3.org/2000/svg'
+    <div className="flex justify-center items-center">
+      <motion.div
+        className="relative flex justify-center items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { ease: easeInOut, duration: 0.4, delay: 2 } }}
       >
-        <motion.circle cx='253' cy='253' r='250' stroke='#00ff99'
-        strokeWidth='4' strokeLinecap='round' strokeLinejoin='round'
-        initial={{strokeDasharray:"24 10 0 0"}}
-        animate={{
-          strokeDasharray:["15 120 25 25","16 25 92 72","4 250 22 22"],
-          rotate:[120,360],
-        }}
-        transition={{
-          repeat:Infinity,
-          repeatType:"reverse",
-          duration:20, 
-        }}
-        />
-      </motion.svg>
-        </motion.div>
-    </div>
-  )
-}
+        {/* Circle */}
+        <motion.svg
+          className="absolute w-[250px] h-[250px] md:w-[450px] md:h-[450px]"
+          fill="transparent"
+          viewBox="0 0 586 586"
+        >
+          <motion.circle
+            cx="293"
+            cy="293"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "reverse",
+              duration: 20,
+            }}
+          />
+        </motion.svg>
 
-export default Photo
+        {/* Image */}
+        <Image
+          src="/ChatGPT Image Apr 16, 2026, 06_46_31 AM.png"
+          alt=""
+          width={350}
+          height={350}
+          className="w-[180px] h-[180px] md:w-[350px] md:h-[350px] rounded-full object-cover"
+        />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Photo;
